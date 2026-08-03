7b16999c8933a403550e42e6969350d96df8f374
Nima <debian860@gmail.com>
2026-08-03 15:46:07 +0800

    kernel/keyboard: ignore key release scancodes

--- kernel/keyboard/keyboard.c
+++ kernel/keyboard/keyboard.c
@@ -6,5 +6,13 @@ volatile uint8_t keyboard_last_scancode = 0;
 
 void keyboard_handler(void)
 {
-    keyboard_last_scancode = inb(0x60);
+    uint8_t scancode = inb(0x60);
+
+    // Ignore key release
+    if (scancode & 0x80)
+    {
+        return;
+    }
+
+    keyboard_last_scancode = scancode;
 }
